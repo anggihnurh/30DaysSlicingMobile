7c90e500d6b7cbe53ce2c035c7e89fff62c48b93
anggihnurh <cickozsiteid@gmail.com>
2022-05-11 23:57:13 +0700

    chore(prettier): setup prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,17 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
   endOfLine: 'auto',
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        'no-inline-styles': false,
+      },
+    ],
+  },
 };
