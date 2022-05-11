e161aa258d489d0a57d7842b15e9d3fd0a990dcd
anggihnurh <cickozsiteid@gmail.com>
2022-05-11 23:57:57 +0700

    chore(babelconfig & jsonconfig): setup import alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@assets/images': './src/assets/images',
+          '@assets/icons': './src/assets/icons',
+          '@pages': './src/pages',
+          '@components': './src/components',
+        },
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.json',
+          '.tsx',
+          '.ts',
+          '.native.js',
+        ],
+      },
+    ],
+  ],
 };
