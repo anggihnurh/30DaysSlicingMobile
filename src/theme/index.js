f197355443fcd81e43e665ded6b7b9070cbfaa02
anggihnurh <cickozsiteid@gmail.com>
2022-05-13 14:42:39 +0700

    feat(themes): adding new themes for consistent design

---
+++ src/theme/index.js
@@ -0,0 +1,56 @@
+const appTheme = {
+  padding: {
+    screen: 16,
+  },
+  text: {
+    'small-reg': {
+      fontFamily: 'Poppins-Regular',
+      fontSize: 14,
+      lineHeight: 16.8,
+      letterSpacing: -0.24,
+    },
+    'normal-reg': {
+      fontFamily: 'Poppins-Regular',
+      fontSize: 16,
+      lineHeight: 19.2,
+      letterSpacing: -0.24,
+    },
+    'normal-med': {
+      fontFamily: 'Poppins-Medium',
+      fontSize: 16,
+      lineHeight: 19.2,
+      letterSpacing: -0.24,
+    },
+    'normal-semi': {
+      fontFamily: 'Poppins-SemiBold',
+      fontSize: 16,
+      letterSpacing: -0.24,
+    },
+    'medium-semi': {
+      fontFamily: 'Poppins-SemiBold',
+      fontSize: 18,
+      letterSpacing: -0.24,
+    },
+    'large-semi': {
+      fontFamily: 'Poppins-SemiBold',
+      fontSize: 20,
+      letterSpacing: -0.24,
+    },
+  },
+  colors: {
+    text1: '#383A44',
+    text2: '#21205A',
+    text3: '#7C809E',
+    primary: '#4378DB',
+    primary2: '#405DB5',
+    secondary: '#FDCD55',
+    secondary2: '#F0A714',
+    tertiary: '#28A164',
+    tertiary2: '#228955',
+    'accent-red': '#F35555',
+    'accent-red2': '#AB3F3F',
+    background: '#F2F5F8',
+  },
+};
+
+export { appTheme };
