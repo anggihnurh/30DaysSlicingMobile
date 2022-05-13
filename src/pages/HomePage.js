a178c6a7ca1c6cba8c2ab515fbbb2a464079d6d2
anggihnurh <cickozsiteid@gmail.com>
2022-05-13 14:47:28 +0700

    feat(homepage): crafting homepage according the design

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,23 @@
+import { Folders, FreeStorageCard, HomeHeader, Searchbar } from '@components';
+import { appTheme } from '@theme';
+import { useView } from '@utils';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 
 export const HomePage = () => {
+  const { getView, view } = useView();
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Page</Text>
+    <View style={{ backgroundColor: appTheme.colors.background }}>
+      <StatusBar
+        backgroundColor={appTheme.colors.background}
+        barStyle="dark-content"
+      />
+      <View onLayout={getView}>
+        <HomeHeader />
+        <Searchbar />
+        <FreeStorageCard />
+      </View>
+      <Folders headerHeight={view.height} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-  },
-  text: {
-    color: '#000',
-  },
-});
